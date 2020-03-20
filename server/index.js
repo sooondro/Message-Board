0365841bfdbb3d4bfe3586ac4ae32b4bf1b7e099
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-20 21:18:38 +0100

    Setting up express

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+//Middleware
+app.use(morgan('tiny')); //Logs server requests like post, get...
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Fullstack message board'
+  });
+});
+
+const port = procces.env.PORT || 1234;
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
